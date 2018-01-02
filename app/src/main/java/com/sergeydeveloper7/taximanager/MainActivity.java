db514155d21435b42979a57b57cb2771f0f81153
Sergey Kozhushany <sergey.kozhushany@faceit-team.com>
2018-01-02 12:26:27 +0200

    Initial commit

---
+++ app/src/main/java/com/sergeydeveloper7/taximanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sergeydeveloper7.taximanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
